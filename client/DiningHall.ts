5c2e1885191abe9a52e6c680453f93b79516f7a3
Malte Müller <malte@malte-mueller.eu>
2021-05-31 22:07:53 +0200

    Add overlay example

--- client/DiningHall.ts
+++ client/DiningHall.ts
@@ -5,9 +5,15 @@ import { Room, RoomAlias } from "./Room";
 
 export class DiningHall extends Room {
 
+    overlay: HTMLDivElement;
+
     constructor(game: Game, canvas: HTMLCanvasElement) {
         super(game, canvas, "diningHall.png");
+        this.overlay = document.getElementById("overlay") as HTMLDivElement;
+        this.overlay.onclick = (e: MouseEvent) => { this.overlay.style.display = "none"; };
+        this.overlay.innerHTML = "<img src=\"Cockpit_nurLampe.png\"></img>"
     }
+
 
     onclick(ev: MouseEvent) {
         let realx = ev.offsetX / this.xfactor;
@@ -15,6 +21,10 @@ export class DiningHall extends Room {
 
         if (realx > 1740 && realx < 2070 && realy > 160 && realy < 820) {
             this.game.nextRoom(RoomAlias.Cockpit);
+        }
+        if (realx > 2150 && realx < 2330 && realy > 155 && realy < 420) {
+            console.log("save!");
+            this.overlay.style.display = "block";
         }
     }
 }
