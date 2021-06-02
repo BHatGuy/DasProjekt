465e3202d3854fb2ee977179d9ead1e6a50c5729
Malte Müller <malte@malte-mueller.eu>
2021-06-02 12:37:56 +0200

    Save popup without moving parts

--- client/DiningHall.ts
+++ client/DiningHall.ts
@@ -4,16 +4,64 @@ import { Room, RoomAlias } from "./Room";
 
 
 export class DiningHall extends Room {
+    imgSafe: CanvasImageSource;
+    imgDisk1: CanvasImageSource;
+    imgDisk2: CanvasImageSource;
+    imgDisk3: CanvasImageSource;
+    imgThingy: CanvasImageSource;
+    popup: HTMLCanvasElement | null = null;
 
 
     constructor(game: Game, canvas: HTMLCanvasElement) {
         super(game, canvas, "diningHall.png");
+        let safe = document.createElement("img");
+        safe.src = "Safe.png";
+        this.imgSafe = safe;
+
+        let d1 = document.createElement("img");
+        d1.src = "Scheibe1.png";
+        this.imgDisk1 = d1;
+
+        let d2 = document.createElement("img");
+        d2.src = "Scheibe2.png";
+        this.imgDisk2 = d2;
+
+        let d3 = document.createElement("img");
+        d3.src = "Scheibe3.png";
+        this.imgDisk3 = d3;
+
+        let t = document.createElement("img");
+        t.src = "Anzeige.png";
+        this.imgThingy = t;
     }
 
     activate() {
         super.activate();
-        this.overlay.innerHTML = "<img src=\"Cockpit_nurLampe.png\"></img>";
-        this.overlay.onclick = (e: MouseEvent) => { this.overlay.style.display = "none"; };
+
+        this.popup = document.createElement("canvas");
+        this.popup.height = window.innerHeight * 0.8;
+        this.popup.width = (window.innerHeight * 0.8) * (4 / 3)
+
+        //this.popup.src = "Safe.png";
+        this.popup.style.display = "block";
+        //this.popup.style.height = "80%";
+        this.popup.style.margin = "4vh auto";
+        this.popup.style.border = "3px solid black";
+        this.overlay.appendChild(this.popup);
+
+        this.overlay.onclick = (e) => { this.overlay.style.display = "none"; };
+    }
+
+    draw(canvas: HTMLCanvasElement) {
+        super.draw(canvas);
+        if (this.popup) {
+            let ctx = this.popup.getContext("2d") as CanvasRenderingContext2D;
+            ctx.drawImage(this.imgSafe, 0, 0, this.popup.width, this.popup.height);
+            ctx.drawImage(this.imgDisk1, 0, 0, this.popup.width, this.popup.height);
+            ctx.drawImage(this.imgDisk2, 0, 0, this.popup.width, this.popup.height);
+            ctx.drawImage(this.imgDisk3, 0, 0, this.popup.width, this.popup.height);
+            ctx.drawImage(this.imgThingy, 0, 0, this.popup.width, this.popup.height);
+        }
     }
 
     deactivate() {
