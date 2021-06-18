5562b826d4fc0db6d4d787b5e8d68eecb52c32b2
Malte Müller <malte@malte-mueller.eu>
2021-06-18 09:35:19 +0200

    Implement opening the save

--- client/DiningHall/Safe.ts
+++ client/DiningHall/Safe.ts
@@ -2,6 +2,7 @@ import { DiningHall } from "./DiningHall";
 
 export class Safe {
     imgSafe: CanvasImageSource;
+    imgSafeOpen: CanvasImageSource;
     imgDisk1: CanvasImageSource;
     imgDisk2: CanvasImageSource;
     imgDisk3: CanvasImageSource;
@@ -9,8 +10,10 @@ export class Safe {
     angles = [this.digitToAngle(0), this.digitToAngle(0), this.digitToAngle(0)];
     goals = [this.digitToAngle(0), this.digitToAngle(0), this.digitToAngle(0)];
     combi = [0, 0, 0];
+    solution = [4, 2, 0];
+    solved = false;
     index = 0;
-    rate = 0.001;
+    rate = 0.0008;
 
     parent: DiningHall;
 
@@ -19,6 +22,10 @@ export class Safe {
         let safe = document.createElement("img");
         safe.src = "Safe.png";
         this.imgSafe = safe;
+
+        let safeOpen = document.createElement("img");
+        safeOpen.src = "Safe_offen.png";
+        this.imgSafeOpen = safeOpen;
 
         let d1 = document.createElement("img");
         d1.src = "Scheibe1.png";
@@ -42,45 +49,52 @@ export class Safe {
         let pxfactor = canvas.width / (this.imgSafe.width as number);
         let pyfactor = canvas.height / (this.imgSafe.height as number);
         ctx.drawImage(this.imgSafe, 0, 0, canvas.width, canvas.height);
-        // TODO: Refactor this
-        ctx.translate(pxfactor * 978, pyfactor * 946);
-        ctx.rotate(this.angles[0]);
-        ctx.translate(-pxfactor * 978, -pyfactor * 946);
-        ctx.drawImage(this.imgDisk1, 0, 0, canvas.width, canvas.height);
-        ctx.resetTransform();
+        if (this.solved) {
+            ctx.drawImage(this.imgSafeOpen, 0, 0, canvas.width, canvas.height);
+        } else {
+            // TODO: Refactor this
+            ctx.translate(pxfactor * 978, pyfactor * 946);
+            ctx.rotate(this.angles[0]);
+            ctx.translate(-pxfactor * 978, -pyfactor * 946);
+            ctx.drawImage(this.imgDisk1, 0, 0, canvas.width, canvas.height);
+            ctx.resetTransform();
 
-        ctx.translate(pxfactor * 978, pyfactor * 946);
-        ctx.rotate(this.angles[1]);
-        ctx.translate(-pxfactor * 978, -pyfactor * 946);
-        ctx.drawImage(this.imgDisk2, 0, 0, canvas.width, canvas.height);
-        ctx.resetTransform();
+            ctx.translate(pxfactor * 978, pyfactor * 946);
+            ctx.rotate(this.angles[1]);
+            ctx.translate(-pxfactor * 978, -pyfactor * 946);
+            ctx.drawImage(this.imgDisk2, 0, 0, canvas.width, canvas.height);
+            ctx.resetTransform();
 
-        ctx.translate(pxfactor * 978, pyfactor * 946);
-        ctx.rotate(this.angles[2]);
-        ctx.translate(-pxfactor * 978, -pyfactor * 946);
-        ctx.drawImage(this.imgDisk3, 0, 0, canvas.width, canvas.height);
-        ctx.resetTransform();
+            ctx.translate(pxfactor * 978, pyfactor * 946);
+            ctx.rotate(this.angles[2]);
+            ctx.translate(-pxfactor * 978, -pyfactor * 946);
+            ctx.drawImage(this.imgDisk3, 0, 0, canvas.width, canvas.height);
+            ctx.resetTransform();
 
-        ctx.drawImage(this.imgThingy, 0, 0, canvas.width, canvas.height);
+            ctx.drawImage(this.imgThingy, 0, 0, canvas.width, canvas.height);
+        }
     }
 
-    update(delta: number): void {       
+    update(delta: number): void {
+        let position = [false, false, false];
 
         for (let i = 0; i < this.goals.length; i++) {
 
             if (this.angles[i] === this.goals[i]) {
+                position[i] = this.combi[i] == this.solution[i];
                 continue;
             } else if (this.angles[i] < this.goals[i]) {
                 this.angles[i] += this.rate * delta;
 
             } else if (this.angles[i] > this.goals[i]) {
                 this.angles[i] -= this.rate * delta;
-
             }
             if (Math.abs(this.angles[i] - this.goals[i]) < this.rate * delta * 0.5) {
                 this.angles[i] = this.goals[i];
             }
         }
+        this.solved = position.reduce((a, b) => a && b);
+        // TODO Pasue before opening
     }
 
     onclick(ev: MouseEvent): void { }
