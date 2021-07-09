c48d5b3ec9ad3c71d06952bddec44e8ca165d8f6
Malte Müller <malte@malte-mueller.eu>
2021-07-09 22:52:04 +0200

    Port UpperHallway

--- client/UpperHallway/UpperHallway.ts
+++ client/UpperHallway/UpperHallway.ts
@@ -1,55 +1,82 @@
 import { Game } from "../Game";
 import { Room, RoomAlias } from "../Room";
-import Flatten from "@flatten-js/core";
+import * as PIXI from 'pixi.js';
 
 
 
 export class UpperHallway extends Room {
 
-    doorMachineRoom: Flatten.Polygon;
-    doorDiningHall: Flatten.Polygon;
-    door2: Flatten.Polygon;
-    doorLowerHallway: Flatten.Polygon;
+    doorMachineRoom: PIXI.Graphics;
+    doorDiningHall: PIXI.Graphics;
+    door2: PIXI.Graphics;
+    doorLowerHallway: PIXI.Graphics;
 
-
-    constructor(game: Game, canvas: HTMLCanvasElement, config: any) {
-        super(game, canvas, config.upperHallway.img);
-        this.doorDiningHall = new Flatten.Polygon(config.upperHallway.doorDiningHall);
-        this.doorMachineRoom = new Flatten.Polygon(config.upperHallway.doorMachineRoom);
-        this.door2 = new Flatten.Polygon(config.upperHallway.door2);
-        this.doorLowerHallway = new Flatten.Polygon(config.upperHallway.doorLowerHallway);
-    }
-
-    onclick(ev: MouseEvent) {
-        let point = this.scale(ev.offsetX, ev.offsetY);
-
-        if (this.doorMachineRoom.contains(point)) {
+    onclickCallback = (data: PIXI.InteractionData) => {
+        console.log(data); 
+        if (data.target === this.doorMachineRoom) {
             this.game.nextRoom(RoomAlias.MachineRoom);
         }
-        if (this.doorDiningHall.contains(point)) {
+        if (data.target === this.doorDiningHall) {
             this.game.nextRoom(RoomAlias.DiningHall);
         }
-        if (this.door2.contains(point)) {
+        if (data.target === this.door2) {
         }
-        if (this.doorLowerHallway.contains(point)) {
+        if (data.target === this.doorLowerHallway) {
             this.game.nextRoom(RoomAlias.LowerHallway);
-        }
+        }       
+    };
+
+    constructor(game: Game) {
+        super(game, game.config.config.upperHallway.img);
+
+        this.doorDiningHall = new PIXI.Graphics();
+        this.doorDiningHall.hitArea = new PIXI.Polygon(game.config.config.upperHallway.doorDiningHall);
+        this.doorDiningHall.interactive = true;
+
+        this.doorMachineRoom = new PIXI.Graphics();
+        this.doorMachineRoom.hitArea = new PIXI.Polygon(game.config.config.upperHallway.doorMachineRoom);
+        this.doorMachineRoom.interactive = true;
+
+        this.door2 = new PIXI.Graphics();
+        this.door2.hitArea = new PIXI.Polygon(game.config.config.upperHallway.door2);
+        this.door2.interactive = true;
+
+        this.doorLowerHallway = new PIXI.Graphics();
+        this.doorLowerHallway.hitArea = new PIXI.Polygon(game.config.config.upperHallway.doorLowerHallway);
+        this.doorLowerHallway.interactive = true;
         
+        this.stage.addChild(this.doorDiningHall, this.doorMachineRoom, this.door2, this.doorLowerHallway);
+
+        this.loadResources();
     }
 
-    onmove(ev: MouseEvent) {
-        super.onmove(ev);
-        let point = this.scale(ev.offsetX, ev.offsetY);
-        // TODO Refactor those kind of things
-        if (this.doorMachineRoom.contains(point) 
-            || this.doorDiningHall.contains(point) 
-            || this.door2.contains(point)
-            || this.doorLowerHallway.contains(point)) {
-            this.canvas.style.cursor = "pointer";
-        } else {
-            this.canvas.style.cursor = "initial";
-        }
+    saveResources(resources: any) {
+        console.log("hello form child");
+        super.saveResources(resources);
+
     }
+
+    activate() {
+        super.activate();
+        this.doorDiningHall.on("click", this.onclickCallback);
+        this.doorMachineRoom.on("click", this.onclickCallback);
+        this.door2.on("click", this.onclickCallback);
+        this.doorLowerHallway.on("click", this.onclickCallback);
+    }
+
+    // onmove(ev: MouseEvent) {
+    //     super.onmove(ev);
+    //     let point = this.scale(ev.offsetX, ev.offsetY);
+    //     // TODO Refactor those kind of things
+    //     if (this.doorMachineRoom.contains(point)
+    //         || this.doorDiningHall.contains(point)
+    //         || this.door2.contains(point)
+    //         || this.doorLowerHallway.contains(point)) {
+    //         this.canvas.style.cursor = "pointer";
+    //     } else {
+    //         this.canvas.style.cursor = "initial";
+    //     }
+    // }
 
 
 }
