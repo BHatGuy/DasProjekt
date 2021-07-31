92f74b4267437e83375633940ae1bc4d4de408f1
Malte Müller <malte@malte-mueller.eu>
2021-07-31 10:57:35 +0200

    First try with animation

--- client/MachineRoom/MachineRoom.ts
+++ client/MachineRoom/MachineRoom.ts
@@ -7,6 +7,7 @@ export class MachineRoom extends Room {
 
     doorBounding: PIXI.Graphics;
     mechanic = new PIXI.Sprite();
+    animation: PIXI.AnimatedSprite | null = null;
 
 
     constructor(game: Game) {
@@ -15,10 +16,14 @@ export class MachineRoom extends Room {
         this.doorBounding.hitArea = new PIXI.Polygon(game.config.machineRoom.door);
         this.doorBounding.interactive = true;
         this.doorBounding.buttonMode = true;
-        
+
         this.doorBounding.on("click", this.onclick);
         this.stage.addChild(this.doorBounding);
 
+        // TODO use config file
+        for (let i = 0; i < 24; i++) {
+            this.loader.add(`animation${i}`, `images/Zeitleiste1_${i}.png`);
+        }
 
         this.loader.add("mechanic", game.config.machineRoom.mechanic);
         this.loadResources();
@@ -28,6 +33,15 @@ export class MachineRoom extends Room {
         super.saveResources(resources);
         this.mechanic = new PIXI.Sprite(resources.mechanic.texture);
         this.stage.addChild(this.mechanic);
+        let frames: Array<PIXI.Texture> = [];
+        for (let i = 0; i < 24; i++) {
+            frames.push(resources[`animation${i}`].texture)
+        }
+        this.animation = new PIXI.AnimatedSprite(frames);
+        this.animation.animationSpeed = 0.2;
+        this.stage.addChild(this.animation);
+        this.animation?.play();
+
     }
 
     onclick = (data: PIXI.InteractionData) => {
